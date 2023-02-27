5289425190d5fbb3b2c162df0ad58370922c587e
ArslanYM <arsalanyaqmalik@gmail.com>
2023-02-27 19:29:27 +0530

    Day1

--- JS/Day1/script.js
+++ JS/Day1/script.js
@@ -1,1 +1,8 @@
-console.log("hello world");
+//variables and data types
+
+var variablename = 'welcome to javascript'; //we can change the value of var variable
+//after es6
+let variablename = 'welcome to javascript again2'; // we can change the value of let variable
+const variablename = 'welcome to javascript again3'; //we can't change the value of const variable
+
+console.log(variablename);
