2cc9968f841a782538e9cd4b2e6788b73328cd2c
ArslanYM <arsalanyaqmalik@gmail.com>
2023-02-27 19:33:11 +0530

    Learning JS

--- JS/Day1/script.js
+++ JS/Day1/script.js
@@ -1,8 +1,18 @@
 //variables and data types
 
-var variablename = 'welcome to javascript'; //we can change the value of var variable
-//after es6
-let variablename = 'welcome to javascript again2'; // we can change the value of let variable
-const variablename = 'welcome to javascript again3'; //we can't change the value of const variable
+//First rule of declaring variables: we can't use reserved keywords.
+//Second rule of declaring variables: we should use meaningful names.
+//Third rule of declaring variables: we can't start a variable name with a number.
+//Fourth rule of declaring variables: we can't use a space or hyphen in a variable name.
+//Fifth rule of declaring variables: we should use camel notation for variable names.
 
-console.log(variablename);
+// It's good practice to use var and let keywords to declare variables.
+
+var variablename1 = 'welcome to javascript'; //we can change the value of var variable
+//after es6
+let variablename2 = 'welcome to javascript again2'; // we can change the value of let variable
+const variablename3  = 'welcome to javascript again3'; //we can't change the value of const variable
+
+console.log(variablename1);
+console.log(variablename2);
+console.log(variablename3);
