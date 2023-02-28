9b142190c389cebe7bc071830acb4269e09f0f9e
ArslanYM <arsalanyaqmalik@gmail.com>
2023-02-28 19:20:35 +0530

    Operators and equality done

---
+++ JS/Day2/script.js
@@ -0,0 +1,56 @@
+//Types of Operators
+//Arithmetic Operators
+// + , - , * , / , % ;
+
+//Comparison Operators
+const a =10;
+const b = 20;
+
+console.log(a > b); //prints false
+console.log(a < b); //prints true
+console.log(a>=b); //prints false
+console.log(a<=b); //prints true
+
+//Is equal to
+console.log(a==b); //prints false
+console.log(a==10); //prints true
+
+//Is not equal to
+console.log(a!=b); //prints true
+console.log(a!=10); //prints false
+
+//Strictly equal to
+//Compares values and data types
+console.log(a===b); //prints false
+
+//Loosely equal to
+//Compares only values
+console.log(5=='5'); //prints true
+
+//The Good ones are === and !==
+//The bad ones are == and !=
+//Always use the good ones
+
+
+//Logical Operators
+// && , || , ! , ^ ;
+
+//Logical AND , && , returns true if both operands are true
+
+console.log(true && true); //prints true
+console.log(true && false); //prints false
+
+//Logical OR , || , returns true if either of the operands is true
+console.log(true || true); //prints true
+console.log(true || false); //prints true
+
+
+//Logical NOT , ! , returns true if the operand is false
+console.log(!true); //prints false
+console.log(!false); //prints true
+
+//Logical XOR , ^ , returns true if either of the operands is true but not both
+
+console.log(true ^ true); //prints false
+console.log(true ^ false); //prints true
+
